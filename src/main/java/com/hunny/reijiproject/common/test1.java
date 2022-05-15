4e233669af8ed8eb3d952152ed960aba8a804d27
hunny <qzj_personal@outlook.com>
2022-05-15 19:25:16 +0800

    Initial Commit

---
+++ src/main/java/com/hunny/reijiproject/common/test1.java
@@ -0,0 +1,8 @@
+package com.hunny.reijiproject.common;
+
+/**
+ * @author QiuZhengJie
+ * @date 2022/5/15
+ */
+public class test1 {
+}
---
+++ src/main/java/com/hunny/reijiproject/common/test2.java
@@ -0,0 +1,16 @@
+package com.hunny.reijiproject.common;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author QiuZhengJie
+ * @date 2022/5/15
+ */
+@Component
+public class test2 {
+
+    public void f()
+    {
+
+    }
+}
